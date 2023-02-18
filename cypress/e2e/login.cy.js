9ca46c5e348d4deab92af3a440a205a6e6674ff5
alfarestaat <fares.tris@gmail.com>
2023-02-18 10:26:27 +0700

    Commit 4

--- cypress/e2e/login.cy.js
+++ cypress/e2e/login.cy.js
@@ -16,4 +16,17 @@ describe('Test Login kasirAja', () => {
       // should contains text "kasirAja"
       cy.contains("kasirAja")
     })
+
+    it('failed login using empty password', () => {
+        cy.visit('https://kasirdemo.belajarqa.com')
+        cy.get('#email').type("fares.tris@gmail.com")
+        cy.contains("login").click()
+    
+        // should be redirected to dashboard page /dashboard
+        cy.url().should('include', '/login')
+    
+        // should contains text "kasirAja"
+        cy.get('#root > div > div > div > div.css-1w7v3tn > div > div.chakra-alert.css-qwanz3').should('have.text','"password" is not allowed to be empty')
+      })
 })
+
